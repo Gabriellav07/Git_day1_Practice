451d5bb22e453929c00588601c496132529998f5
Gabriela <gabil11071996@gmail.com>
2023-04-25 09:45:13 -0400

    pasa's situation about cigarettes

---
+++ src/sprint1/Test1.java
@@ -0,0 +1,14 @@
+package sprint1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String name = "Pasa";
+        int age = 25;
+
+        if(age<=16){
+            System.out.println(name + " not eligible to buy cigarettes");
+        }else{
+            System.out.println(name + " eligible to buy cigarettes");
+        }
+    }
+}
